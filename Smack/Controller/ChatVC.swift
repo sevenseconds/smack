c08b31c0a2d1a7eba71b79277ed0263c792bc869
sevenseconds <sevenseconds@gmail.com>
2018-05-08 18:15:37 +0700

    Lesson 25 - Send Message

--- Smack/Controller/ChatVC.swift
+++ Smack/Controller/ChatVC.swift
@@ -12,10 +12,13 @@ class ChatVC : UIViewController {
     
     @IBOutlet weak var menuBtn: UIButton!
     @IBOutlet weak var channelNameLbl: UILabel!
+    @IBOutlet weak var messageTxtBox: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.bindToKeyboard()
+        let tap = UITapGestureRecognizer(target: self, action: #selector(ChatVC.handleTap))
+        view.addGestureRecognizer(tap)
         menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
@@ -50,6 +53,20 @@ class ChatVC : UIViewController {
         getMessages()
     }
     
+    @IBAction func sendMsgPressed(_ sender: Any) {
+        if AuthService.instance.isLoggedIn {
+            guard let channelId = MessageService.instance.selectedChannel?.id else { return }
+            guard let message = messageTxtBox.text else { return }
+            
+            SocketService.instance.addMessage(messageBody: message, userId: UserDataService.instance.id, channelId: channelId) { (success) in
+                if success {
+                    self.messageTxtBox.text = ""
+                    self.messageTxtBox.resignFirstResponder()
+                }
+            }
+        }
+    }
+    
     func onLoginGetMessages() {
         MessageService.instance.findAllChannel { (success) in
             if success {
@@ -68,5 +85,10 @@ class ChatVC : UIViewController {
         MessageService.instance.findAllMessageForChannel(channelId: channelId) { (success) in
             
         }
+    }
+    
+    @objc
+    func handleTap() {
+        view.endEditing(true)
     }
 }
